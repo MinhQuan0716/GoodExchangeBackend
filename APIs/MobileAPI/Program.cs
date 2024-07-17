502099463ee82a68cc95cbbd16d8699fcf4dd44e
Minhtda <minhtran121202@gmail.com>
2024-07-17 16:56:32 +0700

    cor

--- APIs/MobileAPI/Program.cs
+++ APIs/MobileAPI/Program.cs
@@ -70,9 +70,11 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
     });
 });
 var app = builder.Build();
